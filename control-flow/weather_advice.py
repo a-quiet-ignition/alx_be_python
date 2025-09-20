fc06cd5ca257a8b85d5809d965d9e3e53cbcf5ad
a-quiet-ignition <obatolaraymond@gmail.com>
2025-09-21 00:41:34 +0100

    change conditional from match case to if else

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -4,15 +4,15 @@
 weather = input("What's the weather like today? (sunny/rainy/cold): ")
 
 # Providing clothig recommendations
-match weather:
-    case "sunny":
-        clothing_recommendation = "Wear a t-shirt and sunglasses."
-    case "rainy":
-        clothing_recommendation = "Don't forget your umbrella and a raincoat."
-    case "cold":
-        clothing_recommendation = "Make sure to wear a warm coat and a scarf."
-    case _:
-        clothing_recommendation = "Sorry, I don't have recommendations for this weather."
+
+if weather == "sunny":
+    clothing_recommendation = "Wear a t-shirt and sunglasses."
+elif weather == "rainy":
+    clothing_recommendation = "Don't forget your umbrella and a raincoat."
+elif weather == "cold":
+    clothing_recommendation = "Make sure to wear a warm coat and a scarf."
+else:
+    clothing_recommendation = "Sorry, I don't have recommendations for this weather."
 
 # Displaying the recommendation       
 print(clothing_recommendation)
