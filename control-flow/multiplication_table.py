647b22911e3f91b30fd24a66db1763b886794fd3
a-quiet-ignition <obatolaraymond@gmail.com>
2025-09-21 01:06:16 +0100

    add multiplication_table.py file

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,9 @@
+# A simple multiplication table generator
+
+# Prompting user for a number
+number = int(input("Enter a number to see its multiplication table: "))
+
+# Generating and displaying the multiplication table
+for x in range(1, 10):
+    product = number * x
+    print(number, "*", x, "=", product)
