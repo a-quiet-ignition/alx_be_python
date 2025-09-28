5377f6db2001042ff260bb5b9b63e8ce9846ee43
a-quiet-ignition <obatolaraymond@gmail.com>
2025-09-28 11:17:59 +0100

    change perform_operation args in arithmetic_operation.py file

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,6 +1,9 @@
 # Arithmetic Operations Module
 
-def perform_operation(num1: float, num2: float, operation: str):
+def perform_operation(num1, num2, operation):
+    num1 = float(num1)
+    num2 = float(num2)
+    """the operation to be performed on two numbers."""
     if operation == 'add':
         return num1 + num2
     elif operation == 'subtract':
