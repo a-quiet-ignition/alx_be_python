dda4999532846b3211b96f880b7771cefa5fadde
a-quiet-ignition <obatolaraymond@gmail.com>
2025-10-06 00:22:08 +0100

    add programming_paradigm directory, add bank_account.py fiile, add main-0.py file

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,20 @@
+# A simple class that encapsulates banking operation
+class BankAccount:
+    def __init__(self, account_balance = 0):
+        self.account_balance = account_balance
+        
+        
+    def deposit(self, amount):
+        deposit = self.account_balance + amount
+        return deposit
+    
+    def withdraw(self, amount):
+        if amount > self.account_balance:
+            return False
+        else:
+            withdraw = self.account_balance - amount
+            return withdraw
+        
+    def display_balance(self):
+        return self.account_balance
+        
