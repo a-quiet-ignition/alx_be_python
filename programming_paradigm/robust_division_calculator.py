0a66b69a3daaa6cd480e57164ec90c431ce6be70
a-quiet-ignition <obatolaraymond@gmail.com>
2025-10-06 03:08:26 +0100

    change ValueError message in robust_division_calculator.py file

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -6,6 +6,6 @@ def safe_divide(numerator, denominator):
         return "Error: Cannot divide by zero."  
         
     except ValueError:
-        return "Error: Please enter numeric values only"
+        return "Error: Please enter numeric values only."
     else: 
         return f"The result of the division is {division}"
